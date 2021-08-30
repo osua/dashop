4e844342f4abcf2767e9bd0c644e3ad1fa3739b1
osua <79318129+osua@users.noreply.github.com>
2021-08-30 01:05:48 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,31 +2,36 @@ import React from "react"
 import { Link } from "gatsby"
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <footer className="site-footer">
-                <div className="footer_inner">
-                    <div className="container">
-                        <div className="footer-widget footer-content">
-                            <section id="nav_menu-8" className="widget widget_nav_menu">
-                                <div className="menu-main-container">
-                                    <ul id="menu-main" className="menu">
-                                        <li><Link to="/">Inicio</Link></li>
-                                        <li><Link to="/about">Sobre Nosotros</Link></li>
-                                    </ul>
-                                </div>
-                            </section>
-                        </div>
-                        <div className="footer-bottom social-right-menu ">
-                            <div className="site-info">
-                                ©2021 Dashop. Todos los derechos reservados | <a href="https://github.com/osua/products-gatsby">Osua</a>
-                            </div>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <footer className="site-footer">
+        <div className="footer_inner">
+          <div className="container">
+            <div className="footer-widget footer-content">
+              <section id="nav_menu-8" className="widget widget_nav_menu">
+                <div className="menu-main-container">
+                  <ul id="menu-main" className="menu">
+                    <li>
+                      <Link to="/">Inicio</Link>
+                    </li>
+                    <li>
+                      <Link to="/about">Sobre Nosotros</Link>
+                    </li>
+                  </ul>
                 </div>
-            </footer>
-        )
-    }
+              </section>
+            </div>
+            <div className="footer-bottom social-right-menu ">
+              <div className="site-info">
+                ©2021 Dashop. Todos los derechos reservados |{" "}
+                <a href="https://github.com/osua/products-gatsby">Osu.</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
+  }
 }
 
 export default Footer

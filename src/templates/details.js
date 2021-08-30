cf517643feb6a10cad3ef7514b87362133a9c23a
osua <79318129+osua@users.noreply.github.com>
2021-08-30 00:45:42 -0400

    Update details.js

--- src/templates/details.js
+++ src/templates/details.js
@@ -65,7 +65,7 @@ const ProductDetails = data => (
               data-item-url={`/`}
             >
               <i className="fas fa-tags" />
-              Comprar
+              Comprar Ya!
             </a>
           </div>
         </div>

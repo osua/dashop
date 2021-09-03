bf7fdba39b11139f50211e2d39cb22cb731da42a
osua <79318129+osua@users.noreply.github.com>
2021-09-03 09:24:23 -0400

    ok

--- src/components/header.js
+++ src/components/header.js
@@ -6,27 +6,34 @@ import "bootstrap/dist/css/bootstrap.css"
 import "../css/style.css"
 import logo from "../images/dashop-logo.png"
 
-
-
 const Header = ({ siteTitle }) => (
   <header className="site-header">
     <div className="container">
       <div className="row">
         <div className="col-sm-12 col-md-4 align-self-center">
-          <Link className="header-logo" to="/"><img src={logo} alt="dashop logo"></img></Link>
+          <Link className="header-logo" to="/">
+            <img src={logo} alt="dashop logo"></img>
+          </Link>
         </div>
         <div className="col-sm-12 col-md-8 align-self-center">
           <nav>
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <Link className="nav-link" to="/">Inicio</Link>
+                <Link className="nav-link" to="/">
+                  Inicio
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">Sobre Nosotros</Link>
+                <Link to className="nav-link" to="/about">
+                  Sobre Nosotros
+                </Link>
               </li>
             </ul>
             <div className="header-cart">
-              <Link className="Header__summary snipcart-summary snipcart-checkout" to="#">
+              <Link
+                className="Header__summary snipcart-summary snipcart-checkout"
+                to="#"
+              >
                 <i className="fas fa-cart-plus"></i>
               </Link>
             </div>
@@ -34,8 +41,7 @@ const Header = ({ siteTitle }) => (
         </div>
       </div>
     </div>
-
-  </header >
+  </header>
 )
 
 Header.propTypes = {

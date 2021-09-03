a8ab32d4cdddf45bfbc487f3ac1eaaa52a8b9422
osua <79318129+osua@users.noreply.github.com>
2021-09-03 11:44:26 -0400

    ok

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,9 +10,14 @@ class About extends React.Component {
           <div className="container">
             <div className="row">
               <div className="col-sm-12">
-                <h1>Dashop, el mercado online con Snipcart</h1>
+                <h1>
+                  Dashop, El Mercado Online de productos e Insumos Marinos.
+                </h1>
                 <p>
-                  Ecommerce con Stackeado:{" "}
+                  Ubicados en la famosa Costa Este de Estados Unidos (U.S.A.)
+                  para nuestros clientes en Latino America(Colombia y
+                  Argentina), utilizando Node.JS y Snipcart plataforma de
+                  Ecommerce Full-Stack:{" "}
                   <a href="https://www.gatsbyjs.org/">Gatsby</a> +{" "}
                   <a href="https://flotiq.com">Flotiq</a> +{" "}
                   <a href="https://snipcart.com">Snipcart</a>.
@@ -22,40 +27,27 @@ class About extends React.Component {
                     <p>
                       <strong>
                         Este Marketplace te brindara una serie de Productos
-                        curadoss con la mejor calidad.{" "}
-                        <a href="https://www.gatsbyjs.org/tutorial/">
-                          Arriba podras fijarte en un Paso a paso para recrear
-                          esta tienda.
-                        </a>
-                        .
+                        curadoss con la mejor calidad.
                       </strong>{" "}
-                      Empezaremos sin asumir ningun nivel de tu parte, el camino
-                      y su respectivo paso a paso se encuentran en el tutorial.
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      <strong>
-                        Puedes ir directo a Muestras de codigo, head{" "}
-                        <a href="https://www.gatsbyjs.org/docs/">
-                          en la documentacion de Gatsby.js
-                        </a>
-                        .
-                      </strong>{" "}
-                      En particular puedes revisar las <em>Guias</em>,{" "}
-                      <em>referencia API</em>, y <em> Tutoriales avanzados</em>{" "}
-                      sectiones en la sidebar.
+                      Tambien podras leer documentacion de nuestro Stack
+                      principal{" "}
+                      <a href="https://www.gatsbyjs.org/tutorial/">
+                        Gatsby.js.
+                      </a>
+                      . Utilizados para crear esta tienda.
                     </p>
                   </li>
                 </ul>
 
-                <h2 id="learningflotiq">Utilizando Flotiq</h2>
+                <h2 id="learningflotiq">
+                  Manejamos nuestro contenido con Flotiq
+                </h2>
 
                 <p>
                   Usando Flotiq puedes modelar, crear y Consumir, tu contenido a
-                  tu manera. Flotiq Utiliza structuras de CMS sin cabeza y APIs
-                  en un modelo offline que asegura que tu contenido siempre este
-                  disponible.
+                  tu manera. Flotiq Utiliza structuras de CMS "sin cabeza" y
+                  APIs en un modelo offline que asegura que tu contenido siempre
+                  se encuentre disponible.
                 </p>
                 <p>Mira lo que puedes hacer con Flotiq Headless CMS System:</p>
                 <ul>
@@ -69,28 +61,7 @@ class About extends React.Component {
                       <a href="https://flotiq.com/docs">Flotiq docs</a>
                     </p>
                   </li>
-                  <li>
-                    <p>
-                      <a href="https://github.com/flotiq">
-                        Explora Repositorios de ejemplos.
-                      </a>
-                    </p>
-                  </li>
                 </ul>
-
-                <h2 id="deploy">Despliega</h2>
-                <p>
-                  Este proyeto tambien lo puedes desplegar en Heroku de una
-                  manera sencilla:
-                </p>
-                <p>
-                  <a href="https://heroku.com/deploy?template=https://github.com/flotiq/gatsby-starter-products">
-                    <img
-                      src="https://www.herokucdn.com/deploy/button.svg"
-                      alt="Desplegar"
-                    />
-                  </a>
-                </p>
               </div>
             </div>
           </div>
